eb98397a1338b6b338fbd552f4b5c5b24d0572ad
truongkma <nguyendactruong.kma@gmail.com>
2018-01-09 10:31:12 +0700

    add-homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "homepages#index"
+  devise_for :users, only: [:sessions]
+  use_doorkeeper
 end
